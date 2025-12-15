48988fa9208200ba68086198acf12a8881272455
MatiasSandovalO <115587367+MatiasSandovalO@users.noreply.github.com>
2025-12-15 14:01:30 -0300

    Solucion conexion

--- frontend/app/config.ts
+++ frontend/app/config.ts
@@ -1,1 +1,1 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080';
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://pacheco.chillan.ubiobio.cl:8036';
